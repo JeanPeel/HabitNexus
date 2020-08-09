b825017ee1c184ef953311fe0e9b5126e0e512ca
Jean Peel <Jean.Peel.246@gmail.com>
2020-08-09 03:53:21 -0600

    cleaning up date state

--- src/formElements/Hours/index.js
+++ src/formElements/Hours/index.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import "../style.css";
-// import Moment from 'react-moment';
-// import 'moment-timezone';
-// const moment = require('moment');
 import Select from 'react-select';
 
 var hours = require("../../data/hours");
@@ -11,67 +8,20 @@ var hourData = require("../../data/hour");
 
 class Hours extends React.Component {
 
-    // selectedHour= ""
-
     state = {
-        HabitItem: '',
-        linkstate: "",
-        DateState: "",
-        updateDateState: "",
-        ItemState: "",
-        CategoryState: "",
-        DifficultyState: "",
-        DurrationState: "",
-        PriorityState: "",
-        TODState: "",
         HourState: "",
-        WeekState: "",
-        FormState: "",
-        show: false,
-        newHabit: {
-            PriorityItem: "",
-            linkitem: "",
-            DateItem: "",
-            updateDateItem: "",
-            NameItem: "",
-            CategoryItems: "",
-            DifficultyItem: "",
-            DurrationItem: "",
-            selectedTOD: "",
-            selectedHour: "",
-            WeekSchedule: {
-                Monday: false,
-                Tuesday: false,
-                Wednesday: false,
-                Thursday: false,
-                Friday: false,
-                Saturday: false,
-                Sunday: false
-            }
-
-        }
     }
 
-    newHabit = this.state.newHabit;
-    selectedHour = this.state.newHabit.selectedHour;
     valueB = "";
 
     handleChange2 = (HourState) => {
 
-        const newHabit = this.state.newHabit
-
-        const { selectedHour } = this.newHabit.selectedHour
-
         this.setState({ HourState: HourState });
-        // console.log(`Hour selected on Hours: `, HourState);
+     
         var valueB = HourState.value
-        // console.log(`valueB on Hours: `, valueB);
-
-        this.setState({ newHabit: { selectedHour: HourState.value } });
 
         this.setState({ selectedHour: HourState.value });
 
-        // console.log(`selectedHour on Hours: `, selectedHour);
     };
 
 
@@ -79,18 +29,8 @@ class Hours extends React.Component {
 
         const { HourState } = this.state.HourState;
 
-        const { newHabit } = this.state.newHabit;
-
-        var { selectedHour } = this.state.newHabit.selectedHour;
-
-        // formData.push("HourState: " + this.state.HourState)
-        // formData.unshift("HourState: " + this.state.HourState)
         hourData.unshift(this.state.HourState.value)
-        // formData.pop()
-
-        console.log("Hour State Value: ", this.state.HourState.value)
-        // console.log("newHabit on Hours: ", this.state.newHabit)
-
+       
         return (
             <div>
                 <label htmlFor="Hours">Or Select an Hour</label>
--- src/formElements/ItemDate/index.js
+++ src/formElements/ItemDate/index.js
@@ -1,59 +1,16 @@
 import React, { Component } from "react";
 import "../style.css";
-// import Moment from 'react-moment';
-// import 'moment-timezone';
 const moment = require('moment');
 
 var dateData = require("../../data/date");
 
 class ItemDate extends React.Component {
 
-    DateItem = moment().format("MM/DD/YYYY")
-    // now = moment();
-
-    // console.log("AutoDate: ", AutoDate);
-
     state = {
-        HabitItem: '',
-        linkstate : "",
         DateState: "",
-        updateDateState: "",
-        ItemState: "",
-        CategoryState: "",
-        DifficultyState: "",
-        DurrationState: "",
-        PriorityState: "",
-        TODState: "",
-        HourState: "",
-        WeekState: "",
-        FormState: "",
-        show: false,
-        newHabit: {
-            PriorityItem: "",
-            linkitem: "",
-            DateItem: "",
-            updateDateItem: "",
-            NameItem: "",
-            CategoryItems: "",
-            DifficultyItem: "",
-            DurrationItem: "",
-            selectedTOD: "",
-            selectedHour: "",
-            WeekSchedule: {
-                Monday: false,
-                Tuesday: false,
-                Wednesday: false,
-                Thursday: false,
-                Friday: false,
-                Saturday: false,
-                Sunday: false
-            }
-
-        }
     }
 
     componentDidMount() {
-        // formData.push("DateState: " + AutoDate)
         var AutoDate = moment().format("MM/DD/YYYY")
         dateData.push(AutoDate)
     }
@@ -63,31 +20,18 @@ class ItemDate extends React.Component {
         const value8 = k.target.value
 
         this.setState({ DateState: value8 });
-
-        this.setState({ newHabit: { DateItem: value8 || moment().format("MM/DD/YYYY") } });
     };
 
 
 
 
     render() {
-        // var DateItem = moment().format("MM/DD/YYYY")
+  
         var AutoDate = moment().format("MM/DD/YYYY")
 
         const { DateState } = this.state.DateState;
 
-        const { newHabit } = this.state.newHabit;
-
-        const { DateItem } = this.state.newHabit.DateItem
-
-        // formData.push("DateState: " + AutoDate || "DateState: " + this.state.DateState)
-        // formData.unshift("WeekSchedule: " + this.state.newHabit)
         dateData.unshift(this.state.DateState)
-        // formData.pop()
-        // formData.pop()
-
-        // console.log("newHabit on Date: ", this.state.newHabit)
-
 
         return (
             <div>
@@ -95,7 +39,7 @@ class ItemDate extends React.Component {
                 <input
                     type="date"
                     name="DateItem"
-                    value={DateItem}
+                    value={DateState}
                     onChange={this.updateInputValue6}
                     className="DateItem"
                 />
